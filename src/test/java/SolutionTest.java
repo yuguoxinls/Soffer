075cc3eab526f33872de34dd440bdc41fe31189d
Jack <guoxinyu0617@gmail.com>
2022-10-07 11:12:25 +0800

    leetcode剑指offer练习

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -21,4 +21,39 @@ public class SolutionTest {
 
     }
 
+    @Test
+    public void fib() {
+        Assert.assertEquals(1, solution.fib(2));
+        Assert.assertEquals(5, solution.fib(5));
+        System.out.println(solution.fib(43));
+    }
+
+    @Test
+    public void numWays() {
+        /**
+         * 输入：n = 2
+         * 输出：2
+         * 输入：n = 7
+         * 输出：21
+         * 输入：n = 0
+         * 输出：1
+         */
+        Assert.assertEquals(2, solution.numWays(2));
+        Assert.assertEquals(21, solution.numWays(7));
+        Assert.assertEquals(1, solution.numWays(0));
+    }
+
+    @Test
+    public void minArray() {
+        /**
+         * 输入：numbers = [3,4,5,1,2]
+         *      * 输出：1
+         *      * 输入：numbers = [2,2,2,0,1]
+         *      * 输出：0
+         */
+        int[] numbers = {3,4,5,1,2};
+        Assert.assertEquals(1, solution.minArray(numbers));
+        numbers = new int[]{2, 2, 2, 0, 1};
+        Assert.assertEquals(0, solution.minArray(numbers));
+    }
 }
